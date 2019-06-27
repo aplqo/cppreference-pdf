29e0774c8b99ed5c446251021b26f865ef50659d
aplqo <--global>
2019-06-27 15:05:52 +0800

    Finished parse

---
+++ parse.cpp
@@ -0,0 +1,45 @@
+#include<list>
+#include<fstream>
+#include<string>
+#include<experimental/filesystem>
+#include<gq/Document.h>
+#include<gq/Node.h>
+
+using std::experimental::filesystem::path;
+using std::ifstream;
+using std::string;
+using std::getline;
+
+namespace doc
+{
+	static inline void read(const char *filename,string *result)
+	{
+		ifstream i(filename);
+		if(!i.is_open())
+		{
+			return;
+		}
+		string tmp;
+		while(!i.eof())
+		{
+			getline(i,tmp);
+			(*result)+=tmp;
+			tmp.clear();
+		}
+	}
+	void parse(const char *filename,std::list<std::experimental::filesystem::path> *result)
+	{
+		string s;
+		read(filename,&s);
+		CDocument d;
+		d.parse(s);
+		CSelection sel=d.find("<a>");
+		d.release();
+		size_t num=sel.nodeNum();
+		for(size_t i=0;i<num;i++)
+		{
+			CNode nod=sel.nodeAt(i);
+			result->push_back(nod.attribute("href"));
+		}
+	}
+}
