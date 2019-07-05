bfac00fb11a4dd886766c5755992e009ee01e154
aplqo <aplqo@outlook.com>
2019-07-05 21:24:06 +0800

    Finished error when call link::link(const path&)

--- filelist.h
+++ filelist.h
@@ -11,6 +11,8 @@ namespace doc
         filelist(const std::filesystem::path &p);
         filelist(const char * file);
         ~filelist();
+      private:
+        inline std::filesystem::path toAbsolute(const std::filesystem::path &p);
     };
 }
 
--- link.h
+++ link.h
@@ -15,6 +15,17 @@ namespace doc
         bool Check();
         void Release();
         ~link();
+      private:
+        inline std::filesystem::path toAbsolute(const std::filesystem::path &p);
+        inline bool isFirst(const std::filesystem::path &p); // check if a file should be
+                                            // included in the first time
+        /*---var---*/
+        std::list<std::filesystem::path> *links;
+
+        static link *root;
+        std::list<link> child;
+
+        std::list<std::filesystem::path>::iterator it;
     };
 };
 
